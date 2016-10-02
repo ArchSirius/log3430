5e705138ccf255e6bb46d73df0c4aa75f5335e6b
Samuel Rondeau <samuel.rondeau@polymtl.ca>
2016-10-02 10:27:20 -0400

    Completed SetCalculatorTest

--- TP2/ListeChaineeProject/src/main/SetCalculatorTest.java
+++ TP2/ListeChaineeProject/src/main/SetCalculatorTest.java
@@ -5,224 +5,244 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
-import org.junit.BeforeClass;
 
 public class SetCalculatorTest {
-	
+
 	private SetCalculatorImpl calculator;
-	// First set 
-	private ArrayList<Object> setA;
-	// Second set
-	private ArrayList<Object> setB;
-	// Result set
-	private ArrayList<Object> setR;
-	
-	@BeforeClass
-	public void InitialSetUp(){
-		calculator = new SetCalculatorImpl();
-		setA = new ArrayList<Object>();
-		setB = new ArrayList<Object>();
-		setR = new ArrayList<Object>();
-	}
-	
+	private ArrayList<Object> leftSet;
+	private ArrayList<Object> rightSet;
+	private ArrayList<Object> resultSet;
+
 	@Before
-	public void cleanUp(){
+	public void setUp(){
 		calculator = new SetCalculatorImpl();
-		setA.clear();
-		setB.clear();
-		setR.clear();
+		leftSet = new ArrayList<Object>();
+		rightSet = new ArrayList<Object>();
+		resultSet = new ArrayList<Object>();
 	}
 
 	@Test
 	public void testUnion() {
-		
-		// A = B = {} (ensemble vide)
-		assertEquals("The union of {} and {} must be {}", setR, calculator.union(setA, setB));
-		
-		// A et B sont disjoints
-		setA.add(15);
-		setA.add(2);
-		setB.add(-27);	
-		setR.add(15);
-		setR.add(2);
-		setR.add(-27);	
-		assertEquals("The union of {15,2} and {-27} must be {15,2,-27}", setR, calculator.union(setA, setB));
-		
-		// A = B = non-null
-		setB.clear();
-		setB.add(15);
-		setB.add(2);
-		setR.clear();
-		setR.add(15);
-		setR.add(2);
-		assertEquals("The union of {15,2} and {15,2} must be {15,2}", setR, calculator.union(setA, setB));
-		
-		// A != B
-		setA.add(-4);
-		setB.add(11);
-		setB.add(325);
-		setR.clear();
-		setR.add(15);
-		setR.add(2);
-		setR.add(-4);
-		setR.add(11);
-		setR.add(325);
-		assertEquals("The union of {15,2,-4} and {15,2,11,325} must be {15,2,-4,11,325}", setR, calculator.union(setA, setB));
+		// A = B = {}
+		assertEquals("The union of {} and {} should be {}",
+				resultSet, calculator.union(leftSet, rightSet));
 
-		}
+		// A and B are disjoint sets
+		leftSet.add(2);
+		leftSet.add(15);
+		rightSet.add(-27);
+		resultSet.add(2);
+		resultSet.add(15);
+		resultSet.add(-27);
+		assertEquals("The union of {2, 15} and {-27} should be {-27, 2, 15}",
+				resultSet, calculator.union(leftSet, rightSet));
+
+		// A = B != {}
+		rightSet.clear();
+		rightSet.add(2);
+		rightSet.add(15);
+		resultSet.clear();
+		resultSet.add(2);
+		resultSet.add(15);
+		assertEquals("The union of {2, 15} and {2, 15} should be {2, 15}",
+				resultSet, calculator.union(leftSet, rightSet));
+
+		// A != B joint sets
+		leftSet.add(-4);
+		rightSet.add(11);
+		rightSet.add(325);
+		resultSet.clear();
+		resultSet.add(2);
+		resultSet.add(15);
+		resultSet.add(-4);
+		resultSet.add(11);
+		resultSet.add(325);
+		assertEquals("The union of {-4, 2, 15} and {2, 11, 15, 325} should be {-4, 2, 11, 15, 325}",
+				resultSet, calculator.union(leftSet, rightSet));
+	}
 	
 	@Test
 	public void testIntersection() {
-		
-		// A = B = {} (ensemble vide)
-		assertEquals("The intersection of {} and {} must be {}", setR, calculator.intersection(setA, setB));
-		
-		// A et B sont disjoints (A intersect B = {})
-		setA.add(15);
-		setB.add(-27);
-		setB.add(2);
-		assertEquals("The intersection of {15} and {-27,2} must be {}", setR, calculator.intersection(setA, setB));		
-		
+		// A = B = {}
+		assertEquals("The intersection of {} and {} should be {}",
+				resultSet, calculator.intersection(leftSet, rightSet));
+
+		// A and B are disjoint sets
+		leftSet.add(15);
+		rightSet.add(-27);
+		rightSet.add(2);
+		assertEquals("The intersection of {15} and {-27, 2} should be {}",
+				resultSet, calculator.intersection(leftSet, rightSet));
+
 		// A = B != {}
-		setA.add(2);
-		setA.add(-27);
-		setB.add(15);
-		setR.add(15);
-		setR.add(-27);
-		setR.add(2);
-		assertEquals("The intersection of {15,-27,2} and {-27,2,15} must be {15,-27,2}", setR, calculator.intersection(setA, setB));
-		
-		// A != B et A intersect B != {}
-		setA.add(-4);
-		setB.add(11);
-		setB.add(325);
-		setR.add(2);
-		assertEquals("The intersection of {15,-27,2,-4} and {-27,2,15,11,325} must be {15,-27,2}", setR, calculator.intersection(setA, setB));
+		leftSet.add(-27);
+		leftSet.add(2);
+		rightSet.add(15);
+		resultSet.add(15);
+		resultSet.add(-27);
+		resultSet.add(2);
+		assertEquals("The intersection of {-27, 2, 15} and {-27, 2, 15} should be {-27, 2, 15}",
+				resultSet, calculator.intersection(leftSet, rightSet));
+
+		// A != B joint sets
+		leftSet.add(-4);
+		rightSet.add(11);
+		rightSet.add(325);
+		assertEquals("The intersection of {-27, -4, 2, 15} and {-27, 2, 15, 11, 325} should be {-27, 2, 15}",
+				resultSet, calculator.intersection(leftSet, rightSet));
 	}
-	
+
 	@Test
 	public void testDifference() {
-		
-		// A = B = {} (ensemble vide)
-		assertEquals("The difference of {} and {} must be {}", setR, calculator.difference(setA, setB));
-		
+		// A = B = {}
+		assertEquals("The difference of {} and {} should be {}",
+				resultSet, calculator.difference(leftSet, rightSet));
+
 		// A = B != {}
-		setA.add(15);
-		setA.add(2);
-		setB.add(15);
-		setB.add(2);
-		assertEquals("The difference of {15,2} and {15,2} must be {}", setR, calculator.difference(setA, setB));
-		
-		// A et B sont disjoints (A diff B = A )
-		setB.clear();
-		setB.add(325);
-		setR.add(15);
-		setR.add(2);
-		assertEquals("The difference of {15,2} and {325} must be {15,2}", setR, calculator.difference(setA, setB));
-		
-		// A != B et A diff B != {}
-		setA.add(-27);
-		setA.add(11);
-		setB.add(11);
-		setR.add(-27);
-		assertEquals("The difference of {15,2,-27,11} and {325,11} must be {15,2,-27}", setR, calculator.difference(setA, setB));
+		leftSet.add(2);
+		leftSet.add(15);
+		rightSet.add(2);
+		rightSet.add(15);
+		assertEquals("The difference of {2, 15} and {2, 15} should be {}",
+				resultSet, calculator.difference(leftSet, rightSet));
+
+		// A and B are disjoint sets
+		rightSet.clear();
+		rightSet.add(325);
+		resultSet.add(2);
+		resultSet.add(15);
+		assertEquals("The difference of {2, 15} and {325} should be {2, 15}",
+				resultSet, calculator.difference(leftSet, rightSet));
+
+		// A != B joint sets, LTR
+		leftSet.add(-27);
+		leftSet.add(11);
+		rightSet.add(-27);
+		resultSet.add(11);
+		assertEquals("The difference of {-27, 2, 11, 15} and {11, 325} should be {-27, 2, 15}",
+				resultSet, calculator.difference(leftSet, rightSet));
+
+		// A != B joint sets, RTL
+		resultSet.clear();
+		resultSet.add(325);
+		assertEquals("The difference of {11, 325} and {-27, 2, 11, 15} should be {325}",
+				resultSet, calculator.difference(rightSet, leftSet));
 	}
-	
+
 	@Test
 	public void testSymDifference() {
-		
-		// A = B = {} (ensemble vide)
-		assertEquals("The symetric difference of {} and {} must be {}", setR, calculator.symDifference(setA, setB));
-		
-		// A et B sont disjoints (A diff B = {})
-		setA.add(15);
-		setB.add(2);
-		setB.add(-4);
-		setR.add(15);
-		setR.add(2);
-		setR.add(-4);
-		assertEquals("The symetric difference of {15} and {2,-4} must be {15,2,-4}", setR, calculator.symDifference(setA, setB));
-		
+		// A = B = {}
+		assertEquals("The symetric difference of {} and {} should be {}",
+				resultSet, calculator.symDifference(leftSet, rightSet));
+
+		// A and B are disjoint sets
+		leftSet.add(15);
+		rightSet.add(-4);
+		rightSet.add(2);
+		resultSet.add(15);
+		resultSet.add(-4);
+		resultSet.add(2);
+		assertEquals("The symetric difference of {15} and {-4, 2} should be {-4, 2, 15}",
+				resultSet, calculator.symDifference(leftSet, rightSet));
+
 		// A = B != {}
-		setA.add(2);
-		setA.add(-4);
-		setB.add(15);
-		setR.clear();
-		assertEquals("The symetric difference of {15,2,-4} and {2,-4,15} must be {}", setR, calculator.symDifference(setA, setB));
-		
-		// A != B et A symdiff B = C != {}
-		setA.add(325);
-		setB.add(11);
-		setR.add(325);
-		setR.add(11);
-		assertEquals("The symetric difference of {15,2,-4,325} and {2,-4,15,11} must be {325,11}", setR, calculator.symDifference(setA, setB));	
-		
-		// A != B et B symdiff A = C != {} 
-		setR.clear();
-		setR.add(11);
-		setR.add(325);
-		assertEquals("The symetric difference of {15,2,-4,11} and {2,-4,15,325} must be {11,325}", setR, calculator.symDifference(setA, setB));	
-		
+		leftSet.add(-4);
+		leftSet.add(2);
+		rightSet.add(15);
+		resultSet.clear();
+		assertEquals("The symetric difference of {-4, 2, 15} and {-4, 2, 15} should be {}",
+				resultSet, calculator.symDifference(leftSet, rightSet));
+
+		// A != B joint sets, LTR
+		leftSet.add(325);
+		rightSet.add(11);
+		resultSet.add(325);
+		resultSet.add(11);
+		assertEquals("The symetric difference of {-4, 2, 15, 325} and {-4, 2, 11, 15} should be {11, 325}",
+				resultSet, calculator.symDifference(leftSet, rightSet));
+
+		// A != B joint sets, RTL
+		resultSet.clear();
+		resultSet.add(11);
+		resultSet.add(325);
+		assertEquals("The symetric difference of {-4, 2, 11, 15} and {-4, 2, 15, 325} should be {11, 325}",
+				resultSet, calculator.symDifference(rightSet, leftSet));
 	}
-	
+
 	@Test
 	public void testIsSubset() {
-		
-		// A = B = {} (ensemble vide)
-		setR.add(String.valueOf(true));
-		assertEquals("{} must be a subset of {}", setR, calculator.isSubset(setA, setB));
-		
+		// A = B = {}
+		resultSet.add(String.valueOf(true));
+		assertEquals("{} should be a subset of {}",
+				resultSet, calculator.isSubset(leftSet, rightSet));
+
 		// A = B != {}
-		setA.add(15);
-		setA.add(2);
-		setB.add(15);
-		setB.add(2);
-		assertEquals("{15,2} must be a subset of {15,2}", setR, calculator.isSubset(setA, setB));
-		
-		// A et B sont disjoints (A issub B = {})
-		setB.clear();
-		setB.add(325);
-		setB.add(-27);
-		setR.clear();
-		setR.add(String.valueOf(false));
-		assertEquals("{15,2} must not be a subset of {325,-27}", setR, calculator.isSubset(setA, setB));
-		
-		// A != B et A issub B = 0
-		setB.add(15);
-		assertEquals("{15,2} must not be a subset of {325,-27,15}", setR, calculator.isSubset(setA, setB));
-		
-		// A != B et B issub A = 1
-		setB.add(2);
-		setR.clear();
-		setR.add(String.valueOf(true));
-		assertEquals("{15,2} must not be a subset of {325,-27,15,2}", setR, calculator.isSubset(setA, setB));
+		leftSet.add(2);
+		leftSet.add(15);
+		rightSet.add(2);
+		rightSet.add(15);
+		assertEquals("{2, 15} should be a subset of {2, 15}",
+				resultSet, calculator.isSubset(leftSet, rightSet));
+
+		// A and B are disjoint sets
+		rightSet.clear();
+		rightSet.add(-27);
+		rightSet.add(325);
+		resultSet.clear();
+		resultSet.add(String.valueOf(false));
+		assertEquals("{2, 15} should not be a subset of {-27, 325}",
+				resultSet, calculator.isSubset(leftSet, rightSet));
+
+		// A != B joint sets, LTR
+		rightSet.add(2);
+		rightSet.add(15);
+		resultSet.clear();
+		resultSet.add(String.valueOf(true));
+		assertEquals("{2, 15} should be a subset of {-27, 2, 15, 325}",
+				resultSet, calculator.isSubset(leftSet, rightSet));
+
+		// A != B joint sets, RTL
+		resultSet.clear();
+		resultSet.add(String.valueOf(false));
+		assertEquals("{-27, 2, 15, 325} should not be a subset of {2, 15}",
+				resultSet, calculator.isSubset(rightSet, leftSet));
 	}
-	
+
 	@Test
 	public void testIsSuperset() {
-		
-		// A = B = {} (ensemble vide)
-		setR.add(String.valueOf(true));
-		assertEquals("{} must be a superset of {}", setR, calculator.isSuperset(setA, setB));
+		// A = B = {}
+		resultSet.add(String.valueOf(true));
+		assertEquals("{} should be a superset of {}",
+				resultSet, calculator.isSuperset(leftSet, rightSet));
+
 		// A = B != {}
-		setA.add(-27);
-		setA.add(3);
-		setB.add(3);
-		setB.add(-27);
-		assertEquals("{-27,3} must be a superset of {3,-27}", setR, calculator.isSuperset(setA, setB));
-		// A et B sont disjoints (A issuper B = {})
-		setB.clear();
-		setB.add(15);
-		setR.clear();
-		setR.add(String.valueOf(false));
-		assertEquals("{-27,3} must not be a superset of {15}", setR, calculator.isSuperset(setA, setB));
-		// A != B et A issuper B = 0
-		setB.add(3);
-		assertEquals("{-27,3} must not be a superset of {15,3}", setR, calculator.isSuperset(setA, setB));
-		// A != B et B issuper A = 1
-		setA.add(15);
-		setR.clear();
-		setR.add(String.valueOf(true));
-		assertEquals("{-27,3,15} must be a superset of {15,3}", setR, calculator.isSuperset(setA, setB));
+		leftSet.add(-27);
+		leftSet.add(3);
+		rightSet.add(-27);
+		rightSet.add(3);
+		assertEquals("{-27, 3} should be a superset of {-27, 3}",
+				resultSet, calculator.isSuperset(leftSet, rightSet));
+
+		// A and B are disjoint sets
+		rightSet.clear();
+		rightSet.add(15);
+		resultSet.clear();
+		resultSet.add(String.valueOf(false));
+		assertEquals("{-27, 3} should not be a superset of {15}",
+				resultSet, calculator.isSuperset(leftSet, rightSet));
+
+		// A != B joint sets, LTR
+		rightSet.add(3);
+		leftSet.add(15);
+		resultSet.clear();
+		resultSet.add(String.valueOf(true));
+		assertEquals("{-27, 3, 15} should be a superset of {3, 15}",
+				resultSet, calculator.isSuperset(leftSet, rightSet));
+
+		// A != B joint sets, RTL
+		resultSet.clear();
+		resultSet.add(String.valueOf(false));
+		assertEquals("{3, 15} should not be a superset of {-27, 3, 15}",
+				resultSet, calculator.isSuperset(rightSet, leftSet));
 	}
 }
